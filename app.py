f5c176b38319406ce7a522934e48ef3b309149d7
Ismael Armada <65163077+Archerus35@users.noreply.github.com>
2023-03-08 08:33:10 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pymongo
+from pymongo import MongoClient
+
+# crea una instancia de conexión con la base de datos MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# selecciona la base de datos que quieres usar
+db = client.knoxquack
+
+collection = db.games
+
+# haz una consulta a la colección de games para obtener los primeros 5 juegos
+resultados = db.games.find()
+
+# muestra los resultados en una tabla de Streamlit
+st.write("Los primeros 5 juegos:")
+for resultado in resultados:
+    st.write(resultado)
